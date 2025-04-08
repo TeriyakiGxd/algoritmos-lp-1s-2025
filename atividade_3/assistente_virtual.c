0b2163902f31a927684c19b196f0121ce59a033c
TeriyakiGxd <jdemeiradossantos@gmail.com>
2025-04-08 16:03:18 -0300

    Assistente

--- atividade_3/assistente_virtual.c
+++ atividade_3/assistente_virtual.c
@@ -10,46 +10,72 @@ int main (int argc, char* argv){
         printf("2. Intenso\n");
         scanf ("%d", &tipo);
 
-
-        if (tipo = 1){
-            printf("Escolher genero:\n");
-                int filmeLeve;
-                    printf("1. Comedia\n");
-                    printf("2. Animacao\n");
-            scanf ("%d", &filmeLeve);           
-                if (filmeLeve = 1){
-                printf("Assistir ao filme 'As Branquelas'");
-                if (filmeLeve = 2)
-                printf("Assistir ao filme 'Toy Story'");}
-            
-        if (tipo = 2){
-
-            printf("Escolher genero:\n");
-                int generoIntenso;
-                    printf("1. Terror\n");
-                    printf("2. Acao\n");
-                    scanf ("%d", &generoIntenso);
-
-                    if (generoIntenso = 1){
-                        printf("Escolher subgenero:\n");
-                            int subgeneroTerror;
-                                printf("1. Psicologico\n");
-                                printf("2. Sobrenatural\n");
-                                scanf("%i", &subgeneroTerror);
-                                    if (subgeneroTerror = 1)
-                                        printf("Assistir ao filme 'Corrra'\n");
-                                    if (subgeneroTerror = 2);
-                                        printf("Assistir ao filme 'invocacao do mal'");
-   
-        }
-
-
-    
-
-            return 0;
-
-        }
-
-        return 0;
         
-                        }
+        if (tipo == 1){
+        printf("Escolher genero:\n");
+            int filmeLeve;
+                printf("1. Comedia\n");
+                printf("2. Animacao\n");
+            scanf ("%d", &filmeLeve);
+
+                if (filmeLeve == 1){
+                    printf("Assistir ao filme 'As Branquelas'");
+                    
+                } else if (filmeLeve == 2){
+                    printf("Assistir ao filme 'Toy Story'");
+        
+                } else {
+
+                    printf("opcao invalida!");
+
+                }
+             
+            } else if (tipo == 2){
+                printf("Escolher genero:\n");
+                int filmeIntenso;
+                printf("1. Terror\n");
+                printf("2. Acao\n");
+                scanf ("%d", &filmeIntenso);
+    
+                    if (filmeIntenso == 1){
+                    printf("Escolha o Subgenero\n");
+                    int subTerror;
+                        printf("1. Psicologico\n");
+                        printf("2. Sobreantural\n");
+                        scanf ("%d", &subTerror);
+                            if (subTerror == 1){
+                                printf("Assistir ao filme 'corra'");
+                                
+                            } else if (subTerror == 2){
+                                printf("Assistir ao filme 'Invocacao do Mal'");
+                                
+                            } else {
+
+                             printf("Opcao invalida!");  
+
+                            }
+
+                }   else if (filmeIntenso == 2){
+                    printf("Escolha o Subgenero\n");
+                    int subAcao;
+                        printf("1. Herois\n");
+                        printf("2. Realista\n");
+                        scanf ("%d", &subAcao);
+                            if (subAcao == 1){
+                                printf("Assistir ao filme 'Vingadores: Ultimato'");
+                               
+                            } else if (subAcao == 2){
+                                printf("Assistir ao filme 'Mad Max: Estrada da Furia'");
+                                
+                            } else {
+
+                             printf("Opcao invalida!");  
+
+                            }
+
+                    
+                    }                   
+                }
+return 0; 
+        }
+            
