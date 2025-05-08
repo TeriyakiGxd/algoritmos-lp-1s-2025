f10b728efd8b531bca76a5f9e5db6646c5be0893
TeriyakiGxd <jdemeiradossantos@gmail.com>
2025-05-08 12:09:39 -0300

    :)

---
+++ Praticas(sem_aula)/aeroporto.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+void mainMenu (){
+    int opcao = 0;
+
+    printf("Aeroporto Santos Dumont\n");
+    printf("Escolha uma opcao:\n\n");
+        printf("1. Funcionarios\n");
+        printf("2. Voos\n");
+        printf("3. Aeronaves\n");
+        printf("4. Passageiros\n");
+}
+
+int main(int agrc, char *argv[]){
+    int opcao = 0;
+
+    mainMenu();
+
+return 0;
+};
+
